4c7a9ede925c52693a5ed2b64b4cad9ca52a9d10
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-04 18:40:29 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,8 @@
 # Write your #display_rainbow method here
 def display_rainbow (colors = ['red','orange','yellow','green','blue','indigo','violet'])
   
-  puts "#{colors[0].slice(0.1).capitalize} : #{colors[0]}"
-  puts " : #{colors[1]}"
+  puts "#{colors[0].slice(0.1).capitalize}: #{colors[0]}"
+  puts ": #{colors[1]}"
   puts " : #{colors[2]}"
   puts " : #{colors[3]}"
   puts " : #{colors[4]}"
