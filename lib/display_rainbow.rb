8ce218dacce4b0ae5be4abf0500602db22d51a15
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-04 18:43:04 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,8 +4,8 @@ def display_rainbow (colors = ['red','orange','yellow','green','blue','indigo','
   puts "#{colors[0].slice(0.1).capitalize}: #{colors[0]}"
   puts "#{colors[1].slice(0,1).capitalize}: #{colors[1]}"
   puts "#{colors[2].slice(0,1).capitalize} : #{colors[2]}"
-  puts " : #{colors[3]}"
-  puts " : #{colors[4]}"
-  puts " : #{colors[5]}"
-  puts " : #{colors[6]}"
+  puts "#{colors[3].slice(0,1).capitalize}: #{colors[3]}"
+  puts "#{colors[4].slice(0,1).capitalize}: #{colors[4]}"
+  puts "#{colors[5].slice(0,1).capitalize}: #{colors[5]}"
+  puts "#{colors[6].slice(0,1).capitalize}: #{colors[6]}"
 end
