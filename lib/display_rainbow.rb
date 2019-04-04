a5b43a9dc17f5a9e5df89fdbdef6cc5374955055
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-04 19:19:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,8 @@
 # Write your #display_rainbow method here
 def display_rainbow (colors = ['red','orange','yellow','green','blue','indigo','violet'])
   
-  if(colors == ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']){
-    colors = "#{colors[0].slice(0.1).capitalize}: #{colors[0]}, #{colors[1].slice(0,1).capitalize}: #{colors[1]}, #{colors[2].slice(0,1).capitalize}: #{colors[2]}, #{colors[3].slice(0,1).capitalize}: #{colors[3]}, #{colors[4].slice(0,1).capitalize}: #{colors[4]}, #{colors[5].slice(0,1).capitalize}: #{colors[5]}, #{colors[6].slice(0,1).capitalize}: #{colors[6]}"
-    puts "colors"
-  }
+  
+    puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+    
+  
 end
